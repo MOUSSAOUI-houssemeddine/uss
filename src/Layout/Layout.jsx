c9f9df687d00e5288173421ec5ca8c231f3a3be4
Houssem_moussaoui <houssemeddine.moussaoui8507@etu.univ-setif.dz>
2024-04-27 18:06:09 +0100

    First Commit
    First Date: 2024 M2

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,25 @@
+import React from "react"; 
+
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routers from "../Routes/Routers";
+const Layout = () => {
+
+return(
+<div class="hou">
+<Header />
+<main />
+<Routers />
+<main />
+<Footer />
+</div>
+)
+};
+
+export default Layout;
+
+/*<Header />
+<main />
+<Routers />
+<main />
+<Footer />*/
